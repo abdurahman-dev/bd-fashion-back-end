9573c33bc73d3481ec17a06be31118582e09e781
Abdur Rahman <a.rahman4124@gmail.com>
2021-12-22 20:38:10 +0600

    almost finish

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -19,6 +19,7 @@ res.status(201).json({
 
 exports.getCategory=catchAsyncError(async(req,res,next)=>{
   const cat= await Category.find({})
+  console.log(cat);
   res.status(200).json({
     success:true,
     categories:cat
