d256f53236f5ccf4686072cf89b7deefd15888ca
Abdur Rahman <a.rahman4124@gmail.com>
2021-12-29 17:11:54 +0600

    add

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const app = express();
 //     credentials: true,
 //     origin:"https://db-fashion.vercel.app"
 //  }
-app.use(cors(corsOptions));
+app.use(cors({
+    credentials: true,
+    origin:'http://localhost:3000'
+}));
 app.use(express.json());
 app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({limit: '100mb',extended: true}));
--- config/corsOption.js
+++
@@ -1,4 +0,0 @@
-export const corsOption= {
-    credentials: true,
-    origin:'http://localhost:3000'
-}
