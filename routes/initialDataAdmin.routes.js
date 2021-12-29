3da8e7db9453e0994ece48ec29cfa3658c00ad20
Abdur Rahman <a.rahman4124@gmail.com>
2021-12-29 17:25:08 +0600

    added

--- routes/initialDataAdmin.routes.js
+++ routes/initialDataAdmin.routes.js
@@ -1,10 +1,20 @@
-const express=require('express')
-const { initialDataAdmin } = require('../controllers/initialDataAdmin.controller')
-const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
-var cors = require('cors')
+const express = require('express');
+const {
+  initialDataAdmin,
+} = require('../controllers/initialDataAdmin.controller');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+var cors = require('cors');
 
-const router=express.Router()
+const router = express.Router();
 
-router.route('/initialData').get(cors(),isAuthenticatedUser,authorizeRoles('admin'),initialDataAdmin)
+router.route('/initialData').get(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+  }),
+  isAuthenticatedUser,
+  authorizeRoles('admin'),
+  initialDataAdmin
+);
 
-module.exports=router
+module.exports = router;
