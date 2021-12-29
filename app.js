299c483112d85726a0545aae82b0819347e771da
Abdur Rahman <a.rahman4124@gmail.com>
2021-12-29 13:23:52 +0600

    added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 //     origin:"https://db-fashion.vercel.app"
 //  }
 app.use(cors({
-    credentials: true,
+    credentials: false,
     origin:'http://localhost:3000'
 }));
 app.use(express.json());
