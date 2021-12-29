35d4d1720c83fdcb9d5582c7e77db5dc6d19c042
Abdur Rahman <a.rahman4124@gmail.com>
2021-12-29 13:15:35 +0600

    added

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const errorMiddleWare = require('./middlewares/error');
 const  cors = require('cors')
 
 const app = express();
-
-app.use(cors({
-    credentials: true,
-    origin: "https://db-fashion.vercel.app"
- }));
+//{
+//     credentials: true,
+//     origin:"https://db-fashion.vercel.app"
+//  }
+app.use(cors());
 app.use(express.json());
 app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({limit: '100mb',extended: true}));
